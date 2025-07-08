f752a18c3ed7c07cd843dc7900d08749cecd761c
mani-353 <n200731@rguktn.ac.in>
2025-07-08 17:15:51 +0530

    add env

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,25 @@ const admin = require("firebase-admin");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config(); // Add this at the very top
 
-// Replace with the path to your Firebase service account key
-const serviceAccount = require("./sensor-data.json");
+const serviceAccount = {
+    type: process.env.FB_TYPE,
+    project_id: process.env.FB_PROJECT_ID,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FB_CLIENT_EMAIL,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+};
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://sensor-data-55e63-default-rtdb.firebaseio.com" // Replace this
+    databaseURL: process.env.FB_DB_URL// Replace this 
 });
 
 const db = admin.database();
